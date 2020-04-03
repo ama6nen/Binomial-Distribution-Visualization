c7ea52074ab9c4c22bf531f76dd40b132693871b
Ama <ama6nen@gmail.com>
2020-04-03 17:02:14 +0300

    Add trial success counter and P(X) calculation

--- BinomialDistributionVisualization/form.Designer.cs
+++ BinomialDistributionVisualization/form.Designer.cs
@@ -33,8 +33,19 @@
             this.ProbabilityLabel = new System.Windows.Forms.Label();
             this.ItemsCount = new System.Windows.Forms.NumericUpDown();
             this.CountLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Successes = new System.Windows.Forms.NumericUpDown();
+            this.P_Equal = new System.Windows.Forms.Label();
+            this.P_Less = new System.Windows.Forms.Label();
+            this.P_LessEqual = new System.Windows.Forms.Label();
+            this.P_More = new System.Windows.Forms.Label();
+            this.P_MoreEqual = new System.Windows.Forms.Label();
+            this.FormulaPicture = new System.Windows.Forms.PictureBox();
+            this.ShowFormula = new System.Windows.Forms.CheckBox();
             ((System.ComponentModel.ISupportInitialize)(this.Probability)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.ItemsCount)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Successes)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.FormulaPicture)).BeginInit();
             this.SuspendLayout();
             // 
             // PlotView
@@ -65,14 +76,14 @@
             this.ProbabilityLabel.AutoSize = true;
             this.ProbabilityLabel.Location = new System.Drawing.Point(91, 40);
             this.ProbabilityLabel.Name = "ProbabilityLabel";
-            this.ProbabilityLabel.Size = new System.Drawing.Size(102, 17);
+            this.ProbabilityLabel.Size = new System.Drawing.Size(216, 17);
             this.ProbabilityLabel.TabIndex = 3;
-            this.ProbabilityLabel.Text = "Probability: 50%";
+            this.ProbabilityLabel.Text = "Single trial success probability: 50%";
             // 
             // ItemsCount
             // 
             this.ItemsCount.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.ItemsCount.Location = new System.Drawing.Point(3, 12);
+            this.ItemsCount.Location = new System.Drawing.Point(10, 12);
             this.ItemsCount.Maximum = new decimal(new int[] {
             170,
             0,
@@ -97,17 +108,122 @@
             // CountLabel
             // 
             this.CountLabel.AutoSize = true;
-            this.CountLabel.Location = new System.Drawing.Point(5, 40);
+            this.CountLabel.Location = new System.Drawing.Point(12, 40);
             this.CountLabel.Name = "CountLabel";
-            this.CountLabel.Size = new System.Drawing.Size(61, 17);
+            this.CountLabel.Size = new System.Drawing.Size(57, 17);
             this.CountLabel.TabIndex = 5;
-            this.CountLabel.Text = "Count (n)";
+            this.CountLabel.Text = "Trials (n)";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 97);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 17);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Successes (x)";
+            // 
+            // Successes
+            // 
+            this.Successes.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Successes.Location = new System.Drawing.Point(10, 69);
+            this.Successes.Maximum = new decimal(new int[] {
+            170,
+            0,
+            0,
+            0});
+            this.Successes.Name = "Successes";
+            this.Successes.Size = new System.Drawing.Size(63, 25);
+            this.Successes.TabIndex = 6;
+            this.Successes.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.Successes.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.Successes.ValueChanged += new System.EventHandler(this.Successes_ValueChanged);
+            // 
+            // P_Equal
+            // 
+            this.P_Equal.AutoSize = true;
+            this.P_Equal.Location = new System.Drawing.Point(157, 71);
+            this.P_Equal.Name = "P_Equal";
+            this.P_Equal.Size = new System.Drawing.Size(106, 17);
+            this.P_Equal.TabIndex = 8;
+            this.P_Equal.Text = "P(X = x): 0.00000";
+            // 
+            // P_Less
+            // 
+            this.P_Less.AutoSize = true;
+            this.P_Less.Location = new System.Drawing.Point(269, 71);
+            this.P_Less.Name = "P_Less";
+            this.P_Less.Size = new System.Drawing.Size(106, 17);
+            this.P_Less.TabIndex = 9;
+            this.P_Less.Text = "P(X < x): 0.00000";
+            // 
+            // P_LessEqual
+            // 
+            this.P_LessEqual.AutoSize = true;
+            this.P_LessEqual.Location = new System.Drawing.Point(381, 71);
+            this.P_LessEqual.Name = "P_LessEqual";
+            this.P_LessEqual.Size = new System.Drawing.Size(115, 17);
+            this.P_LessEqual.TabIndex = 10;
+            this.P_LessEqual.Text = "P(X <= x): 0.00000";
+            // 
+            // P_More
+            // 
+            this.P_More.AutoSize = true;
+            this.P_More.Location = new System.Drawing.Point(502, 71);
+            this.P_More.Name = "P_More";
+            this.P_More.Size = new System.Drawing.Size(106, 17);
+            this.P_More.TabIndex = 11;
+            this.P_More.Text = "P(X > x): 0.00000";
+            // 
+            // P_MoreEqual
+            // 
+            this.P_MoreEqual.AutoSize = true;
+            this.P_MoreEqual.Location = new System.Drawing.Point(614, 71);
+            this.P_MoreEqual.Name = "P_MoreEqual";
+            this.P_MoreEqual.Size = new System.Drawing.Size(115, 17);
+            this.P_MoreEqual.TabIndex = 12;
+            this.P_MoreEqual.Text = "P(X >= x): 0.00000";
+            // 
+            // FormulaPicture
+            // 
+            this.FormulaPicture.Image = global::BinomialDistributionVisualization.Properties.Resources.xbinomial_distribution_formula_png_pagespeed_ic_82Np1Wmmm6;
+            this.FormulaPicture.Location = new System.Drawing.Point(12, 327);
+            this.FormulaPicture.Name = "FormulaPicture";
+            this.FormulaPicture.Size = new System.Drawing.Size(420, 242);
+            this.FormulaPicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.FormulaPicture.TabIndex = 13;
+            this.FormulaPicture.TabStop = false;
+            this.FormulaPicture.Visible = false;
+            // 
+            // ShowFormula
+            // 
+            this.ShowFormula.AutoSize = true;
+            this.ShowFormula.Location = new System.Drawing.Point(801, 73);
+            this.ShowFormula.Name = "ShowFormula";
+            this.ShowFormula.Size = new System.Drawing.Size(107, 21);
+            this.ShowFormula.TabIndex = 14;
+            this.ShowFormula.Text = "Show formula";
+            this.ShowFormula.UseVisualStyleBackColor = true;
+            this.ShowFormula.CheckedChanged += new System.EventHandler(this.ShowFormula_CheckedChanged);
             // 
             // form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(927, 581);
+            this.Controls.Add(this.ShowFormula);
+            this.Controls.Add(this.FormulaPicture);
+            this.Controls.Add(this.P_MoreEqual);
+            this.Controls.Add(this.P_More);
+            this.Controls.Add(this.P_LessEqual);
+            this.Controls.Add(this.P_Less);
+            this.Controls.Add(this.P_Equal);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Successes);
             this.Controls.Add(this.CountLabel);
             this.Controls.Add(this.ItemsCount);
             this.Controls.Add(this.ProbabilityLabel);
@@ -120,10 +236,12 @@
             this.MinimizeBox = false;
             this.Name = "form";
             this.ShowIcon = false;
-            this.Text = "Binomial Normal Distribution Visualization";
+            this.Text = "Binomial probability";
             this.Load += new System.EventHandler(this.form_Load);
             ((System.ComponentModel.ISupportInitialize)(this.Probability)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.ItemsCount)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Successes)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.FormulaPicture)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -136,6 +254,15 @@
         private System.Windows.Forms.Label ProbabilityLabel;
         private System.Windows.Forms.NumericUpDown ItemsCount;
         private System.Windows.Forms.Label CountLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown Successes;
+        private System.Windows.Forms.Label P_Equal;
+        private System.Windows.Forms.Label P_Less;
+        private System.Windows.Forms.Label P_LessEqual;
+        private System.Windows.Forms.Label P_More;
+        private System.Windows.Forms.Label P_MoreEqual;
+        private System.Windows.Forms.PictureBox FormulaPicture;
+        private System.Windows.Forms.CheckBox ShowFormula;
     }
 }
 
--- BinomialDistributionVisualization/form.cs
+++ BinomialDistributionVisualization/form.cs
@@ -32,20 +32,45 @@ namespace BinomialDistributionVisualization
             var nMinuskF = (double)Factorial(n - k);
             return nF / (kF * nMinuskF);
         }
-     
+
         void UpdateChart()
         {
             int n = (int)ItemsCount.Value;
+            int x = (int)Successes.Value;
             var nF = (double)Factorial(n); //yeah will break if n too big
             var model = new PlotModel { Title = "Binomial probability distribution" };
             var prob = (double)Probability.Value / 10000.0; //to get to 0-1 probability
             var values = new List<ColumnItem>();
+            double less = 0.0, lessEqual = 0.0, equal = 0.0, more = 0.0, moreEqual = 0.0;
             for (int k = 0; k <= n; k++)
             {
                 //formula for point k: nCk * p^k * (1-p)^(n-k) where n is max items, p is probability of success
                 var weight = Combinations(nF, n, k) * Math.Pow(prob, k) * Math.Pow(1.0 - prob, n - k);
+
+                if (k < x)
+                {
+                    less += weight;
+                    lessEqual += weight;
+                }
+                else if (k == x)
+                {
+                    lessEqual += weight;
+                    moreEqual += weight;
+                    equal = weight;
+                }
+                else if (k > x)
+                {
+                    moreEqual += weight;
+                    more += weight;
+                }
+
                 values.Add(new ColumnItem { Value = weight * 100.0 });
             }
+            P_Equal.Text = $"P(X = x): {Math.Round(equal, 6)}";
+            P_Less.Text = $"P(X < x): {Math.Round(less, 6)}";
+            P_LessEqual.Text = $"P(X <= x): {Math.Round(lessEqual, 6)}";
+            P_More.Text = $"P(X > x): {Math.Round(more, 6)}";
+            P_MoreEqual.Text = $"P(X >= x): {Math.Round(moreEqual, 6)}";
             model.Series.Add(new ColumnSeries { ItemsSource = values });
             model.Axes.Add(new CategoryAxis { Position = AxisPosition.Bottom, TextColor = OxyColors.Transparent }); //hide label
             PlotView.Model = model;
@@ -59,11 +84,18 @@ namespace BinomialDistributionVisualization
 
         private void Probability_ValueChanged(object sender, EventArgs e)
         {
-            ProbabilityLabel.Text = $"Probability: {Math.Round(Probability.Value / 100.0, 2)}%";
+            ProbabilityLabel.Text = $"Single trial success probability: {Math.Round(Probability.Value / 100.0, 2)}%";
             UpdateChart();
         }
 
         private void ItemsCount_ValueChanged(object sender, EventArgs e)
         => UpdateChart();
+
+        private void Successes_ValueChanged(object sender, EventArgs e)
+        => UpdateChart();
+
+        private void ShowFormula_CheckedChanged(object sender, EventArgs e)
+        => FormulaPicture.Visible = !FormulaPicture.Visible;
+
     }
 }
